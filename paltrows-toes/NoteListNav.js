8c17a6095d7fa4509ed1100eb585990f2b2a3fbe
Abdu Jemal <62969593+abduh9350@users.noreply.github.com>
2020-07-05 18:09:30 -0400

    Add files via upload

---
+++ paltrows-toes/NoteListNav.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import CircleButton from './CircleButton'
+import ApiContext from './ApiContext'
+import { countNotesForFolder } from './notes-helpers'
+
+export default class NoteListNav extends React.Component {
+  static contextType = ApiContext;
+
+  render() {
+    const { folders=[], notes=[] } = this.context
+    return (
+      <div className='NoteListNav'>
+        <ul className='NoteListNav__list'>
+          {folders.map(folder =>
+            <li key={folder.id}>
+              <NavLink
+                className='NoteListNav__folder-link'
+                to={`/folder/${folder.id}`}
+              >
+                <span className='NoteListNav__num-notes'>
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div className='NoteListNav__button-wrapper'>
+          <CircleButton
+            tag={Link}
+            to='/add-folder'
+            type='button'
+            className='NoteListNav__add-folder-button'
+          >
+          <br />
+            Folder
+          </CircleButton>
+        </div>
+      </div>
+    )
+  }
+}
